8182b027b0591e72f27538a1539c8b4d7e526cab
cupofc0t <cupofc0t@gmail.com>
2024-10-14 03:03:02 +0800

    feat(core): add dynamic compatible tls&static struct

--- engine/app/bubble_core/build.rs
+++ engine/app/bubble_core/build.rs
@@ -1,19 +1,10 @@
 // Example custom build script.
 fn main() {
-    // println!("cargo::rerun-if-changed=../dyallocator/src/lib.rs");
-    // cargo build the dyallocator crate, then add it's output dll file to lib search path
-    // let output = Command::new("cargo")
-    //     .arg("build")
-    //     .arg("--release")
-    //     .current_dir("../dyallocator")
-    //     .output()
-    //     .expect("Failed to build dyallocator");
-
-    let target_dir_path =
-        "C:\\Users\\sh33p\\Documents\\Repos\\Bubble_engine\\app\\target\\release\\".to_string();
-    println!("cargo:rustc-link-search=native={}", target_dir_path);
-    // #[cfg(target_os = "windows")]
-    // println!("cargo::rustc-link-lib=dylib=dyallocator.{}", DLL_EXTENSION);
-    // #[cfg(not(target_os = "windows"))]
-    // println!("cargo::rustc-link-lib=dylib=dyallocator");
+    let current_path = std::env::current_dir().unwrap();
+    // if it's release build
+    let target_dir_path = current_path.join("../target/release");
+    println!(
+        "cargo:rustc-link-search=native={}",
+        target_dir_path.to_str().unwrap()
+    );
 }
