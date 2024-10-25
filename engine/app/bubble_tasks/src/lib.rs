e82b8d4b71eadf89c09d324e8ac3819293d98d56
cupofc0t <cupofc0t@gmail.com>
2024-10-25 23:30:35 +0800

    feat(tasks): add simple thread core affinity hint (Performance&Efficiency) for testing

--- engine/app/bubble_tasks/src/lib.rs
+++ engine/app/bubble_tasks/src/lib.rs
@@ -1,3 +1,12 @@
 //! Customed async runtime for dynamic shared library
 
 pub use compio::*;
+
+/// The hint for the task system to run the task on the specific thread pool which bind to some specific cores.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum AffinityHint {
+    /// The task should be run on the performance core
+    Performance,
+    /// The task should be run on the efficiency core
+    Efficiency,
+}
