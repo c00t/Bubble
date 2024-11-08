7c84cb734f10b4050824426325b7d6c0bca9930b
cupofc0t <cupofc0t@gmail.com>
2024-11-08 23:56:27 +0800

    fmt

--- engine/app/bubble_alloc/src/lib.rs
+++ engine/app/bubble_alloc/src/lib.rs
@@ -7,7 +7,7 @@
 //!
 //! Currently, tracy memory and tracy log can't be used together. Because tracy-client only accept 1 connection.
 //! It may be fixed in the future by automatically compile tracy-client-sys into a dynamic library.
-//! 
+//!
 //! You should set `TRACY_CLIENT_LIB` and `TRACY_CLIENT_LIB_PATH` to use link TracyClient danamically.
 //! It'll be maintained by Buck2.
 
@@ -15,7 +15,7 @@
 pub mod mimalloc {
     use mimalloc_rust::*;
     use std::alloc::{GlobalAlloc, Layout};
-    
+
     #[cfg(feature = "tracy_memory")]
     use tracy_client::ProfiledAllocator;
 
@@ -25,7 +25,8 @@ pub mod mimalloc {
 
     #[cfg(feature = "tracy_memory")]
     #[global_allocator]
-    static GLOBAL_ALLOC: ProfiledAllocator<GlobalMiMalloc> = ProfiledAllocator::new(GlobalMiMalloc,100);
+    static GLOBAL_ALLOC: ProfiledAllocator<GlobalMiMalloc> =
+        ProfiledAllocator::new(GlobalMiMalloc, 100);
 
     #[allow(improper_ctypes_definitions)]
     #[no_mangle]
@@ -65,16 +66,20 @@ pub mod system {
 
     #[cfg(feature = "tracy_memory")]
     #[global_allocator]
-    static GLOBAL_ALLOC: ProfiledAllocator<Global> = ProfiledAllocator::new(Global,100);
+    static GLOBAL_ALLOC: ProfiledAllocator<Global> = ProfiledAllocator::new(Global, 100);
 
     #[allow(improper_ctypes_definitions)]
     #[no_mangle]
     #[inline]
     extern "C" fn alloc(layout: Layout) -> *mut u8 {
         #[cfg(feature = "tracy_memory")]
-        unsafe { GLOBAL_ALLOC.alloc(layout) }
+        unsafe {
+            GLOBAL_ALLOC.alloc(layout)
+        }
         #[cfg(not(feature = "tracy_memory"))]
-        unsafe { std::alloc::alloc(layout) }
+        unsafe {
+            std::alloc::alloc(layout)
+        }
     }
 
     #[allow(improper_ctypes_definitions)]
@@ -82,9 +87,13 @@ pub mod system {
     #[inline]
     extern "C" fn dealloc(ptr: *mut u8, _layout: Layout) {
         #[cfg(feature = "tracy_memory")]
-        unsafe { GLOBAL_ALLOC.dealloc(ptr, _layout) }
+        unsafe {
+            GLOBAL_ALLOC.dealloc(ptr, _layout)
+        }
         #[cfg(not(feature = "tracy_memory"))]
-        unsafe { std::alloc::dealloc(ptr, _layout) }
+        unsafe {
+            std::alloc::dealloc(ptr, _layout)
+        }
     }
 
     #[allow(improper_ctypes_definitions)]
@@ -92,9 +101,13 @@ pub mod system {
     #[inline]
     extern "C" fn alloc_zeroed(layout: Layout) -> *mut u8 {
         #[cfg(feature = "tracy_memory")]
-        unsafe { GLOBAL_ALLOC.alloc_zeroed(layout) }
+        unsafe {
+            GLOBAL_ALLOC.alloc_zeroed(layout)
+        }
         #[cfg(not(feature = "tracy_memory"))]
-        unsafe { std::alloc::alloc_zeroed(layout) }
+        unsafe {
+            std::alloc::alloc_zeroed(layout)
+        }
     }
 
     #[allow(improper_ctypes_definitions)]
@@ -102,8 +115,12 @@ pub mod system {
     #[inline]
     extern "C" fn realloc(ptr: *mut u8, layout: Layout, new_size: usize) -> *mut u8 {
         #[cfg(feature = "tracy_memory")]
-        unsafe { GLOBAL_ALLOC.realloc(ptr, layout, new_size) }
+        unsafe {
+            GLOBAL_ALLOC.realloc(ptr, layout, new_size)
+        }
         #[cfg(not(feature = "tracy_memory"))]
-        unsafe { std::alloc::realloc(ptr, layout, new_size) }
+        unsafe {
+            std::alloc::realloc(ptr, layout, new_size)
+        }
     }
 }
