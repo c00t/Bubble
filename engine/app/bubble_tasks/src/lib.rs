2e2d4a6da62ed4ec1eecc79806a8b2fa9369b338
cupofc0t <cupofc0t@gmail.com>
2024-12-12 07:33:47 +0800

    feat(tasks): add a sleeper for backon crate

--- engine/app/bubble_tasks/src/lib.rs
+++ engine/app/bubble_tasks/src/lib.rs
@@ -7,8 +7,25 @@ pub use compio::*;
 pub mod task_system_api;
 pub mod types;
 pub use async_ffi;
+pub use backon;
 pub use futures_channel;
 pub use futures_util;
 
 /// A special future
 pub struct LocalFuture<T>(async_ffi::LocalFfiFuture<T>);
+
+pub struct BubbleSleeper;
+
+impl BubbleSleeper {
+    pub fn new() -> Self {
+        Self
+    }
+}
+
+impl backon::Sleeper for BubbleSleeper {
+    type Sleep = runtime::time::Sleep;
+
+    fn sleep(&self, dur: std::time::Duration) -> Self::Sleep {
+        time::sleep_future(dur)
+    }
+}
